75c1c14d174306f4f71ffa37da42a68c5ed636c3
Jami9100 <jamijasmitha9@gmail.com>
2026-03-18 09:40:33 +0530

    OOPS banner using multiple print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,9 +1,13 @@
 
-public class OOPSBannerApp {
+public class OOPSBanner {
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
+        System.out.println("  *****     *****     ******    ****** ");
+        System.out.println(" *     *   *     *   *     *  *       ");
+        System.out.println("*       * *       *  *     *  *       ");
+        System.out.println("*       * *       *  ******    *****  ");
+        System.out.println("*       * *       *  *               *");
+        System.out.println(" *     *   *     *   *         *     *");
+        System.out.println("  *****     *****    *          ***** ");
     }
-}
-
 }
